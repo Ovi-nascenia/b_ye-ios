f24e7e1b2253f8d15a6365d2f4d9226307c6ca9d
nacenia <sudipta@bdipo.com>
2017-05-03 15:04:57 +0600

    start login page

--- Biyeta-Ios/ViewController.swift
+++ Biyeta-Ios/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var _user_name: UITextField!
+    @IBOutlet weak var _userpassword: UITextField!
+    
+    var activityInductor: UIActivityIndicatorView = UIActivityIndicatorView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signInOperation(_ sender: Any) {
+        
+       
+//        activityInductor.startAnimating()
+        
+        activityInductor.center = self.view.center
+        activityInductor.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        self.view.addSubview(activityInductor)
+        
+        let user: String = _user_name.text!
+        
+        let userpassword = _userpassword.text
+        
+        if user == nil || user == "" || userpassword == nil || userpassword == ""
+        {
+            let alert = UIAlertController(title: "Error", message: "Enter User Name and Password", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        else
+        {
+            print("call network operation")
+          
+            activityInductor.startAnimating()
+            ///new work call for login
+        }
+        
+    }
 
 }
 
